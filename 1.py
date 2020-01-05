0168cb56ea08b773124169d9ae5915a8532a7271
EninDmitriy96 <51704019+EninDmitriy96@users.noreply.github.com>
2020-01-06 01:47:55 +0300

    Add files via upload
    Начал писать класс игрока, но он пока не может двигаться по зажатой клавише

--- 1.py
+++ 1.py
@@ -2,10 +2,32 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+WIDTH, HEIGHT = pygame.display.get_surface().get_size()
+FPS = 30
 
 
 class Player(pygame.sprite.Sprite):
-    pass
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+    def move(self, i):
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                self.rect.x -= 5
+            elif i.key == pygame.K_RIGHT:
+                self.rect.x += 5
+            elif i.key == pygame.K_UP:
+                self.rect.y -= 5
+            elif i.key == pygame.K_DOWN:
+                self.rect.y += 5
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -26,10 +48,17 @@ let_sprites = pygame.sprite.Group()
 bonus_sprites = pygame.sprite.Group()
 run = True
 clock = pygame.time.Clock()
+player = Player()
+player_sprites.add(player)
 while run:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        player.move(event)
+    player_sprites.update()
     screen.fill((0, 0, 0))
+    player_sprites.draw(screen)
     pygame.display.flip()
+
 pygame.quit()
