861d166f06f5cdfaee54d80cce0e4983a5dc40ac
AnastasiaFedoseeva <57760202+AnastasiaFedoseeva@users.noreply.github.com>
2020-01-09 08:20:19 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -16,16 +16,21 @@ class Menu:
     def __init__(self):
         self.start_btn = pygame.Surface((300, 200))
         self.exit_btn = pygame.Surface((200, 100))
+        self.records_btn = pygame.Surface((299, 100))
         self.fon = pygame.image.load('data/menu_fon.png')
         self.fon = pygame.transform.scale(self.fon, (WIDTH, HEIGHT))
         self.start = pygame.image.load('data/start.png')
         self.exit = pygame.image.load('data/exit.png')
+        self.records = pygame.image.load('data/records.png')
         self.start_btn.set_colorkey((0, 0, 0))
         self.exit_btn.set_colorkey((0, 0, 0))
+        self.records_btn.set_colorkey((0, 0, 0))
         self.exit_btn_d = False
         self.start_btn_d = False
+        self.records_btn_d = False
         self.k_n_s = False
         self.k_n_e = False
+        self.k_n_r = False
 
     def start_btn_check(self, x, y):
         return x in [i for i in range(100, 400)] and \
@@ -34,6 +39,10 @@ class Menu:
     def exit_btn_check(self, x, y):
         return x in [i for i in range(500, 700)] and \
                y in [i for i in range(650, 750)]
+
+    def records_btn_check(self, x, y):
+        return x in [i for i in range(100, 399)] and \
+               y in [i for i in range(620, 720)]
 
     def draw(self):
         run = True
@@ -54,20 +63,27 @@ class Menu:
                         self.k_n_e = True
                     elif self.start_btn_check(x, y):
                         self.k_n_s = True
+                    elif self.records_btn_check(x, y):
+                        self.k_n_r = True
                     else:
                         self.k_n_s = False
                         self.k_n_e = False
+                        self.k_n_r = False
             if self.k_n_s:
                 self.start = pygame.image.load('data/start1.png')
             elif self.k_n_e:
                 self.exit = pygame.image.load('data/exit1.png')
+            elif self.k_n_r:
+                self.records = pygame.image.load('data/records1.png')
             else:
                 self.start = pygame.image.load('data/start.png')
                 self.exit = pygame.image.load('data/exit.png')
+                self.records = pygame.image.load('data/records.png')
             screen.blit(self.fon, (0, 0))
             win.blit(screen, (0, 0))
             screen.blit(self.start, (100, 350))
             screen.blit(self.exit, (500, 650))
+            screen.blit(self.records, (100, 620))
             pygame.display.update()
 
 
@@ -75,12 +91,15 @@ class PauseMenu:
     def __init__(self):
         self.continue_btn = pygame.Surface((300, 200))
         self.mainmenu_btn = pygame.Surface((200, 100))
+        self.newgame_btn = pygame.Surface((200, 100))
         self.fon = pygame.image.load('data/pause_fon.png')
         self.fon = pygame.transform.scale(self.fon, (WIDTH, HEIGHT))
         self.continuebtn = pygame.image.load('data/continue.png')
         self.mainmenu = pygame.image.load('data/main_menu.png')
+        self.newgame = pygame.image.load('data/newgame.png')
         self.continue_btn.set_colorkey((0, 0, 0))
         self.mainmenu_btn.set_colorkey((0, 0, 0))
+        self.newgame_btn.set_colorkey((0, 0, 0))
 
     def mainmenu_btn_check(self, x, y):
         return x in [i for i in range(100, 400)] and \
@@ -90,18 +109,26 @@ class PauseMenu:
         return x in [i for i in range(500, 700)] and \
                y in [i for i in range(650, 750)]
 
+    def newgame_btn_check(self, x, y):
+        return x in [i for i in range(100, 399)] and \
+               y in [i for i in range(620, 720)]
+
     def draw(self, x, y):
         if self.mainmenu_btn_check(x, y):
             self.mainmenu = pygame.image.load('data/main_menu1.png')
         elif self.continue_btn_check(x, y):
             self.continuebtn = pygame.image.load('data/continue1.png')
+        elif self.newgame_btn_check(x, y):
+            self.newgame = pygame.image.load('data/newgame1.png')
         else:
             self.continuebtn = pygame.image.load('data/continue.png')
             self.mainmenu = pygame.image.load('data/main_menu.png')
+            self.newgame = pygame.image.load('data/newgame.png')
         screen.blit(self.fon, (0, 0))
         win.blit(screen, (0, 0))
         screen.blit(self.mainmenu, (100, 350))
         screen.blit(self.continuebtn, (500, 650))
+        screen.blit(self.newgame, (100, 620))
 
 
 class Player(pygame.sprite.Sprite):
@@ -136,13 +163,13 @@ class Player(pygame.sprite.Sprite):
             self.rect.y += 20
             self.rect.x -= 20
         elif i[pygame.K_LEFT]:
-            self.rect.x -= 10
+            self.rect.x -= 20
         elif i[pygame.K_RIGHT]:
-            self.rect.x += 10
+            self.rect.x += 20
         elif i[pygame.K_UP]:
-            self.rect.y -= 10
+            self.rect.y -= 20
         elif i[pygame.K_DOWN]:
-            self.rect.y += 10
+            self.rect.y += 20
         else:
             g = False
             self.image = pygame.image.load('data/player_s.png')
@@ -190,8 +217,7 @@ while running:
         fon_y = 0
         fon_y1 = -HEIGHT
         lets_c = 10
-        for i in range(lets_c):
-            Let(let_sprites)
+        lets = 0
         while run:
             clock.tick(FPS)
             for event in pygame.event.get():
@@ -211,6 +237,19 @@ while running:
                             break
                         elif pause_menu.continue_btn_check(x, y):
                             pause = False
+                        elif pause_menu.newgame_btn_check(x, y):
+                            player_sprites = pygame.sprite.Group()
+                            enemy_sprites = pygame.sprite.Group()
+                            let_sprites = pygame.sprite.Group()
+                            bonus_sprites = pygame.sprite.Group()
+                            pause = False
+                            run = True
+                            player = Player()
+                            player_sprites.add(player)
+                            fon_y = 0
+                            fon_y1 = -HEIGHT
+                            lets_c = 10
+                            lets = 0
                     pause_menu.draw(x, y)
             if not pause:
                 keys = pygame.key.get_pressed()
@@ -227,6 +266,12 @@ while running:
                 let_sprites.draw(screen)
                 let_sprites.update()
                 player_sprites.draw(screen)
+                if lets != lets_c:
+                    for i in range(5):
+                        Let(let_sprites)
+                    lets += 5
+                else:
+                    enemy = Enemy()
             pygame.display.flip()
     else:
         running = False
